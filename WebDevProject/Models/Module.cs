624b5b19d2c43fcb1412563ea1b92b736f5d7d2f
Sikharin Kitchpanich <bankster007@gmail.com>
2017-10-11 01:21:20 -0800

    Basic layout and style with multiple pages.

---
+++ WebDevProject/Models/Module.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDevProject.Models
+{
+    public class Module
+    {
+        public int Id { get; set; }
+        public ICollection<Topic> topicList { get; set; }
+        public string moduleTitle { get; set; }
+        public string videoURL { get; set; }
+        public string lectureText { get; set; }
+        public int moduleOrder { get; set; }
+    }
+}
