11556d6f06a82fe7ea72b6e45a0395b72a4e92be
Sikharin Kitchpanich <bankster007@gmail.com>
2017-11-10 02:14:58 -0900

    Created login page

--- WebDevProject/Controllers/AccountController.cs
+++ WebDevProject/Controllers/AccountController.cs
@@ -24,6 +24,29 @@ namespace WebDevProject.Controllers
         }
 
         [HttpGet]
+        public IActionResult Login()
+        {
+            ViewBag.Title = "Login Page";
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(vm.Email, vm.Password, vm.RememberMe, false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError("", "Invalid Login Attempt.");
+                return View(vm);
+            }
+            return View(vm);
+        }
+
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
