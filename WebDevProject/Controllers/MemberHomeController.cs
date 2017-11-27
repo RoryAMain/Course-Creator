e413e60e4df5871d632d4d526f5e15020059de4c
Sikharin Kitchpanich <bankster007@gmail.com>
2017-11-26 16:02:23 -0900

    .

--- WebDevProject/Controllers/MemberHomeController.cs
+++ WebDevProject/Controllers/MemberHomeController.cs
@@ -4,18 +4,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using WebDevProject.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace WebDevProject.Controllers
 {
-    [Authorize(Roles = "Member, Admin, Instructor")]
+    //[Authorize(Roles = "Member, Admin, Instructor")]
     public class MemberHomeController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public MemberHomeController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         // GET: /<controller>/
         [Route("MemberHome")]
         public IActionResult Index()
         {
+            var claims = User.Claims;
+
                 return View();
         }
 
