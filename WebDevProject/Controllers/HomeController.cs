f5ac23dd1e32983bb931bb880395db5151682bd2
Sikharin Kitchpanich <bankster007@gmail.com>
2017-11-26 17:20:20 -0900

    Cannot access the index unless the user logs in first

--- WebDevProject/Controllers/HomeController.cs
+++ WebDevProject/Controllers/HomeController.cs
@@ -10,11 +10,13 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace WebDevProject.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
